26af36889ab04ffbfa62a70ac8b788687f818f25
Christoph Sommer <christoph.sommer@ist.ac.at>
2020-07-23 13:29:08 +0200

    refactor and formatting

--- trackpal/features.py
+++ trackpal/features.py
@@ -1,179 +1,305 @@
-import numpy
-import pandas
-from matplotlib import cm
+import pandas as pd
+import numpy as np
+from rdp import rdp
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import pairwise_distances
 from scipy.spatial.distance import squareform
 
 from . import msd, utils, velocity
 
-class TrajectoryFeature(object):
+EPS = 10e-12
+
+
+class Features:
+    def __init__(self, coords, frame):
+        self.coords = coords
+        self.frame = frame
+
+        self._lookup = {}
+        for klass in TrajectoryFeature.__subclasses__():
+            self._lookup[klass.name] = klass
+
+    def get(self, name, **kwargs):
+        return self._lookup[name](coords=self.coords, frame=self.frame)
+
+    @staticmethod
+    def list():
+        for klass in sorted(TrajectoryFeature.__subclasses__(), key=lambda k: k.name):
+            print(klass.name)
+            print(klass.compute.__doc__)
+            print()
+
+
+class TrajectoryFeature:
     def __init__(self, coords, frame):
         self.coords = coords
         self.frame = frame
 
 
 class MSDParabola(TrajectoryFeature):
-    name = "MSD_Parabola"
-    def compute(self, trj, min_trj_len=3, clip=0.9):
+    name = "msd_parabola"
+
+    def compute(self, trj, clip=0.9, min_trj_len=3):
         tau, msd_vals = msd.msd_per_track(trj, self.coords, self.frame)
         d, v2, r2 = 0, 0, 0
         if len(tau) > min_trj_len:
             d, v2, r2 = utils.fit_parabola(tau, msd_vals, clip=clip)
 
-        return pandas.Series({
-                              "MSDparabola_d" : d,
-                              "MSDparabola_v2": v2,
-                              "MSDparabola_r2": r2
-                              })
+        return pd.Series(
+            {f"{self.name}_a": d, f"{self.name}_b": v2, f"{self.name}_fit_r2": r2}
+        )
+
 
 class VACstats(TrajectoryFeature):
-    name = "VAC_stats"
+    name = "vac_stats"
+
     def compute(self, trj, min_trj_len=3):
         displacements = velocity.compute_velocities(trj, self.coords, self.frame)
-        tau, vac_vals = velocity.velocity_autocorr_pre_track(displacements, self.coords, self.frame)
+        tau, vac_vals = velocity.velocity_autocorr_pre_track(
+            displacements, self.coords, self.frame
+        )
 
         if len(tau) > min_trj_len:
-            return pandas.Series({"vac_mean": vac_vals[1:].mean(),
-                                  "vac_std": vac_vals[1:].std(),
-                                  "vac_min": vac_vals[2:].min(),
-                                  "vac_max": vac_vals[2:].max()})
+            return pd.Series(
+                {
+                    f"{self.name}_mean": vac_vals[1:].mean(),
+                    f"{self.name}_std": vac_vals[1:].std(),
+                    f"{self.name}_min": vac_vals[2:].min(),
+                    f"{self.name}_max": vac_vals[2:].max(),
+                }
+            )
         else:
-            return pandas.Series({"vac_mean": 0, "vac_std": 0, "vac_min": 0, "vac_max": 0})
+            return pd.Series(
+                {
+                    f"{self.name}_mean": 0,
+                    f"{self.name}_std": 0,
+                    f"{self.name}_min": 0,
+                    f"{self.name}_max": 0,
+                }
+            )
 
 
 class SpeedStats(TrajectoryFeature):
-    name = "Speed_{}"
+    name = "speed"
+
     def compute(self, trj, min_trj_len=2):
+
         displacements = velocity.compute_velocities(trj, self.coords, self.frame)
 
-        speeds = numpy.linalg.norm(displacements[self.coords], axis=1) / trj[self.frame].diff().dropna()
-
+        speeds = (
+            np.linalg.norm(displacements[self.coords], axis=1)
+            / trj[self.frame].diff().dropna()
+        )
 
         if len(speeds) >= min_trj_len:
             speed_std = speeds.std()
         else:
             speed_std = -1
 
-        return pandas.Series({"Speed_mean": speeds.mean(),
-                              "Speed_std": speed_std,
-                              "Speed_min": speeds.min(),
-                              "Speed_max": speeds.max()})
+        return pd.Series(
+            {
+                f"{self.name}_mean": speeds.mean(),
+                f"{self.name}_std": speed_std,
+                f"{self.name}_min": speeds.min(),
+                f"{self.name}_max": speeds.max(),
+            }
+        )
+
 
 class ConfinementRatio(TrajectoryFeature):
-    name = "ConfinementRatio"
+    name = "confinement_ratio"
+
     def compute(self, trj):
-        return pandas.Series({"Track_confinment_ratio": numpy.linalg.norm(trj.iloc[-1][self.coords]
-                                                                    - trj.iloc[0][self.coords])
-                                                                    / numpy.linalg.norm(trj[self.coords].diff().dropna(), axis=1).sum()})
+        return pd.Series(
+            {
+                f"{self.name}": np.linalg.norm(
+                    trj.iloc[-1][self.coords] - trj.iloc[0][self.coords]
+                )
+                / np.linalg.norm(trj[self.coords].diff().dropna(), axis=1).sum()
+            }
+        )
+
 
 class MeanStraightLineSpeed(TrajectoryFeature):
-    name = "MeanStraightLineSpeed"
+    name = "mean_straight_line_speed"
+
     def compute(self, trj):
-        return pandas.Series({"Track_mean_straight_line_speed": numpy.linalg.norm(trj.iloc[0][self.coords]
-                                                                               - trj.iloc[-1][self.coords])
-                                                                            / (trj.iloc[-1][self.frame]
-                                                                               - trj.iloc[0][self.frame])})
+        return pd.Series(
+            {
+                f"{self.name}": np.linalg.norm(
+                    trj.iloc[0][self.coords] - trj.iloc[-1][self.coords]
+                )
+                / (trj.iloc[-1][self.frame] - trj.iloc[0][self.frame])
+            }
+        )
+
 
 class EllipseFit(TrajectoryFeature):
-    name = "Gyration_tensor"
+    name = "gyration_tensor"
+
     def compute(self, trj):
         p = trj[self.coords].values
         pc = p - p.mean(0)
 
-        cov = numpy.cov(pc.T)
-        evals, evecs = numpy.linalg.eig(cov)
+        cov = np.cov(pc.T)
+        evals, evecs = np.linalg.eig(cov)
 
-        sort_indices = numpy.argsort(evals)
+        sort_indices = np.argsort(evals)
 
-        l1 = numpy.sqrt( evals[sort_indices[0]])
-        l2 = numpy.sqrt( evals[sort_indices[1]])
+        l1 = np.sqrt(evals[sort_indices[0]])
+        l2 = np.sqrt(evals[sort_indices[1]])
 
-        return pandas.Series({"Gyration_tensor_minor_axis_length" : l1,
-                              "Gyration_tensor_major_axis_length" : l2,
-                              "Gyration_tensor_axis_ratio"        : l1/(l2+10e-9),
-                              "Gyration_tensor_radius"            : l1**2 + l2**2,
-                              "Gyration_tensor_coherence"         : ((l2-l1) / (l2+l1))**2
-                              }
-                              )
+        return pd.Series(
+            {
+                f"{self.name}_minor_axis_length": l1,
+                f"{self.name}_major_axis_length": l2,
+                f"{self.name}_axis_ratio": l1 / (l2 + EPS),
+                f"{self.name}_radius": l1 ** 2 + l2 ** 2,
+                f"{self.name}_coherence": ((l2 - l1) / (l2 + l1)) ** 2,
+            }
+        )
+
 
 class TrackDuration(TrajectoryFeature):
-    name = "Track_duration"
+    name = "track_duration"
+
     def compute(self, trj):
-        return pandas.Series({"Track_duration": (1 + trj.iloc[-1][self.frame] - trj.iloc[0][self.frame])})
+        return pd.Series(
+            {f"{self.name}": (1 + trj.iloc[-1][self.frame] - trj.iloc[0][self.frame])}
+        )
+
 
 class TrackLength(TrajectoryFeature):
-    name = "Track_length"
+    name = "track_length"
+
     def compute(self, trj):
-        return pandas.Series({"Track_length": numpy.linalg.norm(trj[self.coords].diff().dropna(), axis=1).sum()})
+        return pd.Series(
+            {
+                f"{self.name}": np.linalg.norm(
+                    trj[self.coords].diff().dropna(), axis=1
+                ).sum()
+            }
+        )
+
+
+class VelocityAverage(TrajectoryFeature):
+    name = "velocity_average"
+
+    def compute(self, trj):
+        tl = TrackLength(self.coords, self.frame).compute
+        td = TrackDuration(self.coords, self.frame).compute
+        return pd.Series(
+            {"velocity_average": tl(trj).track_length / td(trj).track_duration}
+        )
+
 
 class TrackDiameter(TrajectoryFeature):
     name = "track_diameter_stats"
+
     def compute(self, trj):
         pw = pairwise_distances(trj[self.coords].values)
         sf = squareform(pw, checks=False)
 
-        return pandas.Series({"Track_diameter_mean"  : sf.mean(),
-                              "Track_diameter_max"   : sf.max(),
-                              "Track_diameter_std"   : sf.std(),
-                             })
+        return pd.Series(
+            {
+                f"{self.name}_mean": sf.mean(),
+                f"{self.name}_max": sf.max(),
+                f"{self.name}_std": sf.std(),
+            }
+        )
+
 
 def angles_from_displacements(disp_xy):
     a = []
-    eps = 10e-12
-    for i in range(len(disp_xy)-1):
+    for i in range(len(disp_xy) - 1):
         v0 = disp_xy[i, :]
-        v1 = disp_xy[i+1, :]
-        c = (v0 @ v1) / (numpy.linalg.norm(v0) * numpy.linalg.norm(v1) + eps)
-        ang = numpy.arccos(numpy.clip(c, -1, 1))
+        v1 = disp_xy[i + 1, :]
+        c = (v0 @ v1) / (np.linalg.norm(v0) * np.linalg.norm(v1) + EPS)
+        ang = np.arccos(np.clip(c, -1, 1))
 
         a.append(ang)
     return a
 
+
+class RDP_simplification(TrajectoryFeature):
+    name = "rdp_simplification"
+
+    def compute(self, trj, min_trj_len=3, eps=1):
+        trj_rdp = rdp(trj[self.coords].values, epsilon=eps)
+        displ_xy = np.diff(trj_rdp, axis=0)
+        ang = angles_from_displacements(displ_xy)
+
+        if len(ang) > min_trj_len:
+            m = np.mean(ang)
+            s = np.std(ang)
+        else:
+            m, s = 0, 0
+
+        return pd.Series(
+            {
+                f"{self.name}_ratio_{eps}": len(trj_rdp) / len(trj),
+                f"{self.name}_directional_change_mean_{eps}": m,
+                f"{self.name}_directional_change_std_{eps}": s,
+            }
+        )
+
+
 class DirectionalChange(TrajectoryFeature):
-    name = "directional_change"
+    name = "directional_change_stats"
+
     def compute(self, trj):
         disp_xy = trj[self.coords].diff().dropna().values
 
         a = angles_from_displacements(disp_xy)
 
         if len(a) > 1:
-            p1 = numpy.percentile(a, 25)
-            p2 = numpy.percentile(a, 50)
-            p3 = numpy.percentile(a, 75)
+            p1 = np.percentile(a, 25)
+            p2 = np.percentile(a, 50)
+            p3 = np.percentile(a, 75)
         else:
             p1 = 0
             p2 = 0
             p3 = 0
 
-        return pandas.Series({"Track_directional_change_mean" : numpy.mean(a),
-                              "Track_directional_change_std"  : numpy.std(a),
-                              "Track_directional_change_25_percentile"   : p1,
-                              "Track_directional_change_median"   : p2,
-                              "Track_directional_change_75_percentile"   : p3,
-                             })
+        return pd.Series(
+            {
+                f"{self.name}_mean": np.mean(a),
+                f"{self.name}_std": np.std(a),
+                f"{self.name}_25_percentile": p1,
+                f"{self.name}_median": p2,
+                f"{self.name}_75_percentile": p3,
+            }
+        )
+
 
 class DirectionalChangeCount(TrajectoryFeature):
     name = "directional_change_count"
+
     def compute(self, trj):
         disp_xy = trj[self.coords].diff().dropna().values
 
         a = angles_from_displacements(disp_xy)
 
-        res = pandas.Series({"Track_directional_change_greater_{}_degree".format(d) : (numpy.array(a) > numpy.deg2rad(d)).sum() / len(a) for d in [20,45,90]})
+        res = pd.Series(
+            {
+                f"{self.name}_greater_{d}_degree": (np.array(a) > np.deg2rad(d)).sum()
+                / len(a)
+                for d in [20, 45, 90]
+            }
+        )
         return res
 
 
-
 class PartitionFeature(TrajectoryFeature):
-    name = "Moving_tracklet"
-    def compute(self, trj, eps=6, t_scale=1.):
+    name = "moving_tracklet"
+
+    def compute(self, trj, eps=6, t_scale=1.0):
 
         p = trj[self.coords].values
         t = trj[self.frame].values
-        dt = numpy.diff(t)
+        dt = np.diff(t)
 
-        labels = partition_dbscann(p, t, eps, t_scale=t_scale, ) + 1
+        labels = partition_dbscann(p, t, eps, t_scale=t_scale,) + 1
 
         from skimage.measure import label
         from scipy.ndimage.morphology import binary_dilation
@@ -183,46 +309,52 @@ class PartitionFeature(TrajectoryFeature):
         unique_labels = set(labels)
         for u in unique_labels:
             if u > 0:
-                sub_track_indecies.append(numpy.nonzero(labels == u)[0])
+                sub_track_indecies.append(np.nonzero(labels == u)[0])
 
-        sub_track_center_idx  = [int(sti.mean()) for sti in sub_track_indecies]
-        sub_track_center_pos  = [p[c, :] for c in sub_track_center_idx]
+        sub_track_center_idx = [int(sti.mean()) for sti in sub_track_indecies]
+        sub_track_center_pos = [p[c, :] for c in sub_track_center_idx]
         sub_track_center_time = [t[c] for c in sub_track_center_idx]
 
         if len(unique_labels) > 2:
-            between_times = numpy.diff(sub_track_center_time)
-            between_dists = numpy.linalg.norm(numpy.diff(sub_track_center_pos, axis=0), axis=1)
+            between_times = np.diff(sub_track_center_time)
+            between_dists = np.linalg.norm(
+                np.diff(sub_track_center_pos, axis=0), axis=1
+            )
 
         else:
             between_times = 0
             between_dists = 0
 
         res = {
-                "Dwell_state_count" : labels.max(),
-                "Dwell_state_total_duration" : (dt * (labels[1:] > 0)).sum(),
-                "Dwell_state_relative_duration" : (dt * (labels[1:] > 0)).sum() / dt.sum(),
+            f"{self.name}_dwell_state_count": labels.max(),
+            f"{self.name}_dwell_state_total_duration": (dt * (labels[1:] > 0)).sum(),
+            f"{self.name}_dwell_state_relative_duration": (dt * (labels[1:] > 0)).sum()
+            / dt.sum(),
+            f"{self.name}_dwell_between_state_duration_mean": np.mean(between_times),
+            f"{self.name}_dwell_between_state_duration_max": np.max(between_times),
+            f"{self.name}_dwell_between_state_length_mean": np.mean(between_dists),
+            f"{self.name}_dwell_between_state_length_max": np.max(between_dists),
+        }
 
-                "Dwell_between_state_duration_mean" : numpy.mean(between_times),
-                "Dwell_between_state_duration_max"  : numpy.max(between_times),
-                "Dwell_between_state_length_mean" : numpy.mean(between_dists),
-                "Dwell_between_state_length_max"  : numpy.max(between_dists),
-                }
-
-        labels = labels==0
+        labels = labels == 0
         labels = remove_small_objects(labels, 3)
         labels = label(labels)
-
 
         sub_track_indecies = []
         unique_labels = set(labels)
         for u in unique_labels:
             if u > 0:
-                sub_track_indecies.append(numpy.nonzero(labels == u)[0])
+                sub_track_indecies.append(np.nonzero(labels == u)[0])
 
-        sub_track_pos  = [p[sti, :] for sti in sub_track_indecies]
+        sub_track_pos = [p[sti, :] for sti in sub_track_indecies]
         sub_track_time = [t[sti] for sti in sub_track_indecies]
 
-        sub_trajectoris = [pandas.DataFrame({self.coords[0] : p[:,0], self.coords[1] : p[:,1], self.frame:t}) for p,t in zip(sub_track_pos, sub_track_time)]
+        sub_trajectoris = [
+            pd.DataFrame(
+                {self.coords[0]: p[:, 0], self.coords[1]: p[:, 1], self.frame: t}
+            )
+            for p, t in zip(sub_track_pos, sub_track_time)
+        ]
 
         sub_trajectoris = [st for st in sub_trajectoris if len(st > 3)]
 
@@ -230,69 +362,56 @@ class PartitionFeature(TrajectoryFeature):
             sub_trajectoris = [trj]
 
         SS = SpeedStats(self.coords, self.frame)
-        speed_vals = pandas.concat([SS.compute(st) for st in sub_trajectoris], axis=1)
+        speed_vals = pd.concat([SS.compute(st) for st in sub_trajectoris], axis=1)
         for key, vals in speed_vals.iterrows():
             res[f"{self.name}__{key}__mean"] = vals.mean()
-            # res[f"{self.name}__{key}__std"]  = vals.std()
-
-
 
         ELF = EllipseFit(self.coords, self.frame)
-        elf_vals = pandas.concat([ELF.compute(st) for st in sub_trajectoris], axis=1)
+        elf_vals = pd.concat([ELF.compute(st) for st in sub_trajectoris], axis=1)
 
         for key, vals in elf_vals.iterrows():
             res[f"{self.name}__{key}__mean"] = vals.mean()
-            # res[f"{self.name}__{key}__std"]  = vals.std()
-
 
         # Directonal change
-        dc_vals = numpy.array([numpy.mean(angles_from_displacements(st[self.coords].diff().dropna().values))
-                                                for st in sub_trajectoris])
+        dc_vals = np.array(
+            [
+                np.mean(
+                    angles_from_displacements(st[self.coords].diff().dropna().values)
+                )
+                for st in sub_trajectoris
+            ]
+        )
 
-        res[f"{self.name}__Directional_change_mean__mean"] = dc_vals.mean()
-        # res[f"{self.name}__Directional_change_mean__std"]  = dc_vals.std()
+        res[f"{self.name}__directional_change_mean__mean"] = dc_vals.mean()
+
+        return pd.Series(res)
 
 
-
-        return pandas.Series(res)
-
-
-def partition_dbscann(p, t, eps, t_scale=1.):
-    X = numpy.c_[p, t * t_scale]
+def partition_dbscann(p, t, eps, t_scale=1.0):
+    X = np.c_[p, t * t_scale]
     clustering = DBSCAN(eps).fit(X)
     labels = clustering.labels_
 
     return labels
 
 
+# helper function for visualizaiton
 def partition_get_moving_part(trj, eps, t_scale, coords, frame):
-
     p = trj[coords].values
     t = trj[frame].values
-    dt = numpy.diff(t)
+    dt = np.diff(t)
 
-    labels = partition_dbscann(p, t, eps, t_scale=t_scale, ) + 1
+    labels = partition_dbscann(p, t, eps, t_scale=t_scale,) + 1
 
     from skimage.measure import label
     from scipy.ndimage.morphology import binary_dilation
     from skimage.morphology import remove_small_objects
 
-
-
-    labels = labels==0
+    labels = labels == 0
     labels = remove_small_objects(labels, 3)
 
     new_trj = trj[labels]
     new_trj[frame] = list(range(len(new_trj)))
 
-    if len(new_trj) == 0:
-        print("aASDFASDFASDF")
-
     return new_trj[coords + [frame]]
-
-
-
-
-
-
 
--- trackpal/read.py
+++ trackpal/read.py
@@ -1,11 +1,23 @@
-import numpy; np=numpy
-import pandas; pd=pandas
+import numpy
+
+np = numpy
+import pandas
+
+pd = pandas
 import pathlib
 from xml.etree import cElementTree as ET
 
-def tm_xml_tracks(fn):
+
+def trackmate_xml_tracks(fn):
     """Reads tracks from trackmate xml track file and returns a DataFrame
-    plus other additional data info """
+       plus other additional data info
+
+    Args:
+        [str] fn (file name): [description]
+
+    Returns:
+        [type]: [description]
+    """
 
     tracks = ET.parse(fn)
     frame_interval = float(tracks.getroot().attrib["frameInterval"])
@@ -13,24 +25,33 @@ def tm_xml_tracks(fn):
     space_units = str(tracks.getroot().attrib["spaceUnits"])
 
     attributes = []
-    for ti, track in enumerate(tracks.iterfind('particle')):
-        for spots in track.iterfind('detection'):
-            attributes.append([ti, int(spots.attrib.get('t')),
-                                   float(spots.attrib.get('x')),
-                                   float(spots.attrib.get('y'))])
+    for ti, track in enumerate(tracks.iterfind("particle")):
+        for spots in track.iterfind("detection"):
+            attributes.append(
+                [
+                    ti,
+                    int(spots.attrib.get("t")),
+                    float(spots.attrib.get("x")),
+                    float(spots.attrib.get("y")),
+                ]
+            )
 
-    track_table = pd.DataFrame(attributes, columns=['TRACK_ID','FRAME','POSITION_X','POSITION_Y'])
-    track_table['POSITION_T'] = track_table["FRAME"] * frame_interval
+    track_table = pd.DataFrame(
+        attributes, columns=["TRACK_ID", "FRAME", "POSITION_X", "POSITION_Y"]
+    )
+    track_table["POSITION_T"] = track_table["FRAME"] * frame_interval
 
     return track_table, frame_interval, time_units, space_units
 
+
 def imaris_tracks(fn):
     """Reads tracks from images csv file and returns a DataFrame"""
-    data = pandas.read_csv(fn, skiprows=3, sep=",", )
+    data = pandas.read_csv(fn, skiprows=3, sep=",",)
     data = data[data.TrackID.notna()]
     cols = ["Position X", "Position Y", "Position Z", "Time"]
 
-    for c in cols: data[c] = pandas.to_numeric(data[c])
+    for c in cols:
+        data[c] = pandas.to_numeric(data[c])
     data = data.sort_values(["TrackID", "Time"])
 
     data["FRAME"] = data["Time"]
@@ -48,16 +69,18 @@ def imaris_tracks(fn):
 
 def imaris_tracks_custom(in_dir):
     import warnings
+
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
 
         root = pathlib.Path(in_dir)
         all_data = []
         trackid = 0
-        for d in root.glob('**/*'):
+        for d in root.glob("**/*"):
             if d.is_file() and d.name.endswith(".csv"):
                 treatment = d.stem.split("_")[-1]
-                if treatment == "Control": treatment = treatment.lower()
+                if treatment == "Control":
+                    treatment = treatment.lower()
 
                 print("Reading file", d.name)
                 data = imaris_tracks(d.absolute())
@@ -69,7 +92,7 @@ def imaris_tracks_custom(in_dir):
 
                     cur_data = data.loc[data["TrackID"] == tid]
                     cur_data["TrackID"] = trackid
-                    trackid +=1
+                    trackid += 1
 
                     all_data.append(cur_data)
 
