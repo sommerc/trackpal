08d8d29576b2901c71adf9671dfe2aeb26bb39a0
Christoph Sommer <christoph.sommer@ist.ac.at>
2020-07-23 13:29:31 +0200

    added rdp dep.

--- setup.py
+++ setup.py
@@ -4,29 +4,31 @@ from setuptools import setup
 with open("README.md", "rb") as f:
     description = f.read().decode("utf-8")
 
-with open('trackpal/version.py', "r") as f:
+with open("trackpal/version.py", "r") as f:
     exec(f.read())
 
 setup(
-    name = "TrackPal",
-    packages = ["trackpal"],
-    version = __version__,
-    description = description,
-    long_description = description,
-    long_description_content_type='text/markdown',
-    url='https://git.ist.ac.at/csommer/trackpal',
-#    entry_points = {'console_scripts': []},
-    author = "Christoph Sommer",
-    author_email = "christoph.sommer23@gmail.com",
+    name="TrackPal",
+    packages=["trackpal"],
+    version=__version__,
+    description=description,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://git.ist.ac.at/csommer/trackpal",
+    #    entry_points = {'console_scripts': []},
+    author="Christoph Sommer",
+    author_email="christoph.sommer23@gmail.com",
     install_requires=[
-            "numpy",
-            "pandas>=1.0.4",
-            "scikit_image",
-            "scikit_learn",
-            "tifffile",
-            "tqdm",
-            "scipy",
-            "statsmodel",
-            "matplotlib"
-      ]
-    )
+        "numpy",
+        "pandas>=1.0.4",
+        "scikit_image",
+        "scikit_learn",
+        "tifffile",
+        "tqdm",
+        "scipy",
+        "statsmodels",
+        "matplotlib",
+        "rdp",
+    ],
+)
+
