e12f1714c8bdbf870ad984410ffd85d0aff5dabc
Christoph Sommer <christoph.sommer@ist.ac.at>
2020-07-23 13:35:11 +0200

    impro. dtype

--- trackpal/simulate.py
+++ trackpal/simulate.py
@@ -9,9 +9,9 @@ trackid = "TrackID"
 def brownian(
     n_tracks=20, min_time=0, max_time=42, diffusion=1, x_rng=(0, 100), y_rng=(0, 100)
 ):
-    t_id = 0
+
     res = []
-    for k_ in range(n_tracks):
+    for t_id in range(n_tracks):
 
         x_off = np.random.rand() * (x_rng[1] - x_rng[0]) + x_rng[0]
         y_off = np.random.rand() * (y_rng[1] - y_rng[0]) + x_rng[1]
@@ -20,14 +20,13 @@ def brownian(
         pos_xy = np.cumsum(pos_xy, axis=0) + [y_off, x_off]
 
         frames = np.arange(min_time, max_time + 1, dtype=np.int32)[..., None]
-        track_id = np.ones((len(frames), 1), dtype=np.int32)
+        track_ids = np.ones((len(frames), 1), dtype=np.int32) * t_id
 
-        df = pd.DataFrame(
-            np.c_[track_id * t_id, frames, pos_xy], columns=[trackid, frame] + coords
-        )
+        df = pd.DataFrame(pos_xy, columns=coords)
+        df.insert(0, frame, frames)
+        df.insert(0, trackid, track_ids)
 
         res.append(df)
-        t_id += 1
 
     brownian_track = pd.concat(res, axis=0)
 
