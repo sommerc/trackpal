bb4f2b84c86380e6b1a909088ed03ecdd6451e7e
Christoph Sommer <christoph.sommer@ist.ac.at>
2020-07-24 10:35:43 +0200

    add linear demodata gen

--- trackpal/simulate.py
+++ trackpal/simulate.py
@@ -7,7 +7,13 @@ trackid = "TrackID"
 
 
 def brownian(
-    n_tracks=20, min_time=0, max_time=42, diffusion=1, x_rng=(0, 100), y_rng=(0, 100)
+    n_tracks=20,
+    min_time=0,
+    max_time=42,
+    diffusion=1,
+    x_rng=(0, 100),
+    y_rng=(0, 100),
+    frame_interval=1,
 ):
 
     res = []
@@ -16,11 +22,49 @@ def brownian(
         x_off = np.random.rand() * (x_rng[1] - x_rng[0]) + x_rng[0]
         y_off = np.random.rand() * (y_rng[1] - y_rng[0]) + x_rng[1]
 
-        pos_xy = np.random.randn(max_time - min_time + 1, 2) * diffusion
+        pos_xy = np.random.randn(max_time - min_time + 1, 2) * np.sqrt(
+            2 * diffusion * frame_interval
+        )
         pos_xy = np.cumsum(pos_xy, axis=0) + [y_off, x_off]
 
-        frames = np.arange(min_time, max_time + 1, dtype=np.int32)[..., None]
-        track_ids = np.ones((len(frames), 1), dtype=np.int32) * t_id
+        frames = np.arange(min_time, max_time + 1, dtype=np.int32) * frame_interval
+        track_ids = np.ones(len(frames), dtype=np.int32) * t_id
+
+        df = pd.DataFrame(pos_xy, columns=coords)
+        df.insert(0, frame, frames)
+        df.insert(0, trackid, track_ids)
+
+        res.append(df)
+
+    brownian_track = pd.concat(res, axis=0)
+
+    return brownian_track
+
+
+def linear(
+    n_tracks=20,
+    min_time=0,
+    max_time=42,
+    velocity=1,
+    x_rng=(0, 100),
+    y_rng=(0, 100),
+    frame_interval=1,
+):
+
+    res = []
+    for t_id in range(n_tracks):
+
+        x_off = np.random.rand() * (x_rng[1] - x_rng[0]) + x_rng[0]
+        y_off = np.random.rand() * (y_rng[1] - y_rng[0]) + x_rng[1]
+
+        frames = np.arange(min_time, max_time + 1, dtype=np.int32) * frame_interval
+        track_ids = np.ones(len(frames), dtype=np.int32) * t_id
+
+        xy_direction = np.random.randn(1, 2)
+        xy_direction = xy_direction / np.linalg.norm(xy_direction) * velocity
+
+        pos_xy = np.tile(xy_direction, (len(frames), 1))
+        pos_xy = np.cumsum(pos_xy, axis=0) + [y_off, x_off]
 
         df = pd.DataFrame(pos_xy, columns=coords)
         df.insert(0, frame, frames)
--- trackpal/utils.py
+++ trackpal/utils.py
@@ -1,9 +1,10 @@
 import pandas
-import numpy; np = numpy
+import numpy
+
+np = numpy
 import statsmodels.api as sm
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
-
 
 
 def defdict2array(defdict, agg=np.mean):
@@ -15,54 +16,57 @@ def defdict2array(defdict, agg=np.mean):
     return tau, arr
 
 
-
-
 def abline(intercept, slope, col="k", label=""):
     """Plot a line from slope and intercept"""
     axes = plt.gca()
     x_vals = np.array(axes.get_xlim())
     y_vals = intercept + slope * x_vals
-    plt.plot(x_vals, y_vals, col+'--', label=label)
+    plt.plot(x_vals, y_vals, col + "--", label=label)
+
 
 def parabola(t, D, V):
-    return D*t + V*(t**2)
+    return D * t + V * (t ** 2)
+
 
 def fit_parabola(x, y, clip=0.25):
-    if 0<clip<=1:
+    if 0 < clip <= 1:
         clip_int = int(len(x) * clip)
     else:
         clip_int = int(clip)
 
-
     x = x[:clip_int]
     y = y[:clip_int]
 
-    (D, V2), cov = curve_fit(parabola, x, y, p0=(1,1), bounds=[(-numpy.inf,-numpy.inf), (numpy.inf, numpy.inf)])
+    (D, V2), cov = curve_fit(
+        parabola,
+        x,
+        y,
+        p0=(1, 1),
+        bounds=[(-numpy.inf, -numpy.inf), (numpy.inf, numpy.inf)],
+    )
 
     residuals = y - parabola(x, D, V2)
-    ss_res = numpy.sum(residuals**2)
-    ss_tot = numpy.sum((y-numpy.mean(y))**2)
+    ss_res = numpy.sum(residuals ** 2)
+    ss_tot = numpy.sum((y - numpy.mean(y)) ** 2)
     r2 = 1 - (ss_res / ss_tot)
-
 
     return D, V2, r2
 
 
 def fit_line(taus, msd, sem, clip_first=0.25):
-    if 0<clip_first<1:
-        clip_int = int(len(taus) * clip_first)-1
+    if 0 < clip_first < 1:
+        clip_int = int(len(taus) * clip_first) - 1
     else:
         clip_int = int(clip_int)
 
     # clip data for fit to only use first part
-    X =      taus[:clip_int]
-    Y =  msd[:clip_int]
-    W = 1/sem[:clip_int]
+    X = taus[:clip_int]
+    Y = msd[:clip_int]
+    W = 1 / sem[:clip_int]
 
     # weighted LS
     X = sm.add_constant(X)
     wls_model = sm.WLS(Y, X, weights=W)
     fit_params = wls_model.fit().params
     return fit_params
-
 
