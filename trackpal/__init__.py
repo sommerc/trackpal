c85ea0d302301559a722d14380544533def5ef42
Christoph Sommer <christoph.sommer@ist.ac.at>
2020-08-06 11:28:33 +0200

    link image

--- trackpal/__init__.py
+++ trackpal/__init__.py
@@ -31,9 +31,14 @@ The default identifiers are:
 ```python
 import trackpal as tp
 
-trj = tp.simulate.brownian(n_tracks=1)
-trj[trj.id.xy] # xy coordinates of track
+trj = tp.simulate.brownian_linear(n_tracks=10)
+
+# plot as lines
+trj.groupby(trj.trackid).apply(
+    tp.visu.plot_trj, coords=trj.xy, line_fmt=".-",
+)
 ```
+![](https://git.ist.ac.at/csommer/trackpal/-/blob/master/doc/img/bl_tracks_01.png "Output")
 
 ### General
 For most computations trackpal relies on pandas `groupby` and `apply` mechanism.
@@ -45,47 +50,10 @@ excellent projects [TrackMate](https://imagej.net/TrackMate),
 
 ## Examples:
 
-### Simulate and plot tracks
+### Track features
 
-```python
-import pandas as pd
-import trackpal as tp
+* Simulate different motion types and compute track feautres [notebook](https://git.ist.ac.at/csommer/trackpal/-/blob/master/examples/01_track_features.ipynb)
 
-trj_brownian = tp.simulate.brownian(n_tracks=10)
-trj_linear = tp.simulate.brownian_linear(n_tracks=10)
-
-trj_brownian["label"] = 0
-trj_linear["label"] = 1
-
-# concatenate and relabel
-trj = tp.concat_relabel([trj_linear, trj_brownian])
-
-# plot as lines
-trj.groupby(trj.trackid).apply(
-    tp.visu.plot_trj, coords=trj.coords, line_fmt=".-",
-)
-
-# prepare feature factory
-feature_factory = tp.features.Features(frame=trj.frameid, coords=trj.coords)
-
-# compute two features
-conf_ratio = feature_factory.get("confinement_ratio")
-speed_stats = feature_factory.get("speed_stats")
-
-conf_ratio_res = trj.groupby(trj.trackid).apply(conf_ratio.compute)
-speed_stats_res = trj.groupby(trj.trackid).apply(speed_stats.compute)
-
-# retrieve labels assignment
-y = trj.groupby(trj.trackid)["label"].first()
-
-# merge into single DataFrame
-features = pd.concat([conf_ratio_res, speed_stats_res, y], axis=1)
-
-# plot with pandas
-features.plot.scatter(
-    x="confinement_ratio", y="speed_stats_mean", c="label", cmap="coolwarm"
-)
-```
 
 ## Source code and issue tracker:
 [on github](https://www.github.com/sommerc/trackpal)
